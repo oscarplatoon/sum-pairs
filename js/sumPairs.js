1f988eec9ad9fbbb5c12f7e179e2d536aa5aa889
ver2point0 <jmci10@hotmail.com>
2021-05-28 09:16:20 -0700

    sumPairs js solution

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,40 @@
-exports.sumPairs = function() {
+// const sumPairs = (arr, target) => {
+//     let sum = 0;
+//     let answerArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             sum = arr[i] + arr[j];
+//             if (sum === target) {
+//                 answerArr.push([arr[i], arr[j]])
+//             }
+//         }
+//     }
+//     if (answerArr.length) {
+//         return answerArr;
+//     } else return 'unable to find pairs';
+// }
 
+const sumPairs = (arr, target) => {
+    let answerArr = [];
+    let sortedArr = arr.sort();
+    let right = sortedArr.length - 1;
+    let left = 0;
+    while (left < right) {
+        let addition = sortedArr[right] + sortedArr[left];
+        if (addition === target) {
+            answerArr.push([sortedArr[left], sortedArr[right]]);
+            right--;
+        } else if (addition < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+
+    if (answerArr.length) {
+        return answerArr;
+    } else return 'unable to find pairs';
 };
+
+module.exports = sumPairs;
