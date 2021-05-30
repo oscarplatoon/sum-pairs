957f9aa2481db3fe5d66febd41d7bd4c9ef3f66a
Kyle Gunter <kylewgunter@gmail.com>
2021-05-30 17:26:55 -0500

    Kyle JS V2

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,29 @@
-exports.sumPairs = function() {
+const sumPairs = (arr, num) => {
 
+  let answerArr = [];
+  let arrSort = arr.sort();
+
+  let start = 0;
+  let end = arrSort.length - 1;
+
+  while (start < end) {
+    if (arrSort[start] + arrSort[end] > num){
+      end--
+    }
+    else if (arrSort[start] + arrSort[end] < num){
+      start++
+    } 
+    else {
+      answerArr.push([arrSort[start], arrSort[end]])
+      end--
+    }
+  }
+  //console.log(answerArr)
+  if(answerArr.length){
+    return answerArr
+  } else {
+    return 'unable to find pairs'
+  }
 };
+
+module.exports = sumPairs
