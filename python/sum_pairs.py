08b3700d7df8e0b9e5a474c0345dcbfaf5a04b45
Kyle Gunter <kylewgunter@gmail.com>
2021-05-31 01:13:43 -0500

    Kyle Python v1

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,31 @@
-def sum_pairs():
+def sum_pairs(list, num):
+    list_pairs = []
+    
+    start = 0
+    end = -1
+
+    while start < end:
+        counter = list[start] + list[end]
+        if counter == num:
+            list_pairs.extend(list[start], list[end])
+            # print('if', counter)
+        elif counter < num:
+            start = start + 1
+            # ('elif', counter)
+        else: 
+            end = end - 1
+
+    if len(list_pairs):
+        return list_pairs
+    else:
+        return 'unable to find pairs'
+
+    # print(list_pairs)    
+        
+    #print(type(list[start]), type(list[end]), list[start], list[end], list[start + 1], list[end - 1])
+
+    #print(start, end)
+    
+    #print(list[start + 1], list[end - 1], list, list_pairs)
+
+sum_pairs([1,2,3,4,5], 9) # [[4,5]]
